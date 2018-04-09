24cf3300da15b9610e0833e1f98d3755730283da
Andrey N. Petrov <andreynpetrov@gmail.com>
2018-04-09 19:20:16 +0300

    auto intermediate

--- s3-backup.py
+++ s3-backup.py
@@ -70,7 +70,8 @@ def copy_to_bucket(args, key):
         key = ".".join(l)
     print("Start upload to bucket: %s. Key: %s" % (args.bucket, key))
     s3.upload_file(args.tar_filename, args.bucket, key)
-    shutil.rmtree(args.source)
+    for i in args.source:
+        shutil.rmtree(i)
     os.remove(args.tar_filename)
     print("Finished.")
 
